97a593ea32433d5148c16e312ed4f0d014b93e2a
XIAXI <baocai@duotin.com>
2017-09-02 12:25:51 +0800

    萨达

--- xnw_backmanager/src/test/Mapper/MapperTest.java
+++ xnw_backmanager/src/test/Mapper/MapperTest.java
@@ -2,6 +2,7 @@ package Mapper;
 
 import com.xnw.dao.mapper.test.StudentMapper;
 import com.xnw.dao.model.test.Student;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -9,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by BaoCai on 17/9/2.
@@ -19,19 +22,23 @@ import java.util.Date;
 @ContextConfiguration(locations = { "classpath:application.xml"})
 public class MapperTest {
 
-    @Autowired
+    @Resource
     private StudentMapper studentMapper;
 
     @Autowired
     private SqlSessionFactoryBean sqlSessionFactory;
 
     @Test
-    public void testMapper(){
+    public void testMapper() throws Exception {
         Student student = new Student();
         student.setName("nie datou");
         student.setSex(2);
         student.setCreatedAt(new Date());
         student.setUpdatedAt(new Date());
+
+        SqlSession session = sqlSessionFactory.getObject().openSession();
+
+        List object = (List) session.selectOne("select * from student where id = 9");
         studentMapper.insert(student);
     }
 }
